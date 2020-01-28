8a02e9b0f1d1a6e2726fa9709326216a60a04a3f
Amadeu Anderlin Neto <neto.amadeu@gmail.com>
2020-01-28 16:33:56 -0400

    Adicionado Swagger ao projeto.

--- src/main/java/com/anderlin/pontointeligente/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/anderlin/pontointeligente/api/security/config/WebSecurityConfig.java
@@ -59,7 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.authorizeRequests()
-				.antMatchers("/auth/**")
+				.antMatchers("/auth/**", "/api/cadastrar-pj", "/api/cadastrar-pf", "/v2/api-docs",
+						"/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
 				.permitAll().anyRequest().authenticated();
 		
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
