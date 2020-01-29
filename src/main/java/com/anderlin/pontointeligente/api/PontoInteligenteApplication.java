ab1babef8c60e8e017d5d0f363ba70e154d9d756
Amadeu Anderlin Neto <neto.amadeu@gmail.com>
2020-01-29 11:01:16 -0400

    Adicionado EhCache.

--- src/main/java/com/anderlin/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/anderlin/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,10 +2,12 @@ package com.anderlin.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 //desabilita a autenticação
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @SpringBootApplication
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
